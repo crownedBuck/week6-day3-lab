2cf27ca1e3ee35e4b4c6c48aa6c370f1a3f4fd5b
Laura Hart <onemansavedall@hotmail.com>
2024-02-28 14:27:16 -0600

    first commit for this set of files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(`${__dirname}/public`))
+
+app.listen(4000, () => console.log(
+    'So long and thank you for the fish'
+))
+
+
